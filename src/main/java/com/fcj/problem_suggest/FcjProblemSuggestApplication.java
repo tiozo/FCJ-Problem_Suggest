e8aa8e6c40f3dde2fdf65213f09b184ad768772a
tiozo <azinz850@gmail.com>
2025-09-22 20:19:44 +0700

    initial commit for project

---
+++ src/main/java/com/fcj/problem_suggest/FcjProblemSuggestApplication.java
@@ -0,0 +1,13 @@
+package com.fcj.problem_suggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcjProblemSuggestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcjProblemSuggestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fcj/problem_suggest/FcjProblemSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcj.problem_suggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcjProblemSuggestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
